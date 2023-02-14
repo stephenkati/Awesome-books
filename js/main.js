64df5d4fe04c7d9aea5a13defed82f50eb901623
stephenkati <stephenkatuli88@gmail.com>
2023-02-14 12:17:30 +0300

    Fix Javascript eror

--- js/main.js
+++ js/main.js
@@ -1,53 +1,58 @@
 const bookList = document.querySelector('#bookList');
-const addBtn = document.querySelector('#addBtn')
-const title = document.querySelector('#title')
-const arthur = document.querySelector('#arthur')
+const addBtn = document.querySelector('#addBtn');
+const title = document.querySelector('#title');
+const arthur = document.querySelector('#arthur');
 
 const d = new Date();
-document.querySelector("#current-time").innerHTML = d;
+document.querySelector('#current-time').innerHTML = d;
 
 let bookObj = [
-   
-]
-addBtn.addEventListener('click',function(){
-    if(title.value) {
-    let titleValue = title.value
-    let arthurValue = arthur.value
-    const newBook = new Object()
-     newBook.bookTitle = titleValue
-     newBook.arthur = arthurValue
-     bookObj.push(newBook)
-     let objVar = ''
-     for(let i = 0;i < bookObj.length;i+=1) {
-         bookObj[i].id = i + 1
-        objVar += `<li><p><span>${bookObj[i].bookTitle}</span> by <span>${bookObj[i].arthur}</span> </p> <button id=${bookObj[i].id.toString()} onClick="removeBook(this.id)" >Remove</button></li>`
-     }
-     bookList.innerHTML = objVar
-     localStorage.setItem('inputArr',JSON.stringify(bookObj))
+
+];
+addBtn.addEventListener('click', () => {
+  if (title.value) {
+    const titleValue = title.value;
+    const arthurValue = arthur.value;
+    const newBook = {};
+    newBook.bookTitle = titleValue;
+    newBook.arthur = arthurValue;
+    bookObj.push(newBook);
+    let objVar = '';
+    for (let i = 0; i < bookObj.length; i += 1) {
+      bookObj[i].id = i + 1;
+      objVar += `<li><p><span>${bookObj[i].bookTitle}</span> by <span>${bookObj[i].arthur}</span> </p> <button id=${bookObj[i].id} onClick="removeBook(${bookObj[i].id})" >Remove</button></li>`;
     }
-})
+    bookList.innerHTML = objVar;
+    localStorage.setItem('inputArr', JSON.stringify(bookObj));
+  }
+});
 
-function removeBook(id){
-   let filteredArr = bookObj.filter(item => item.id != id)
-   bookObj = filteredArr
-   objVar = ''
-   for(let i = 0;i < bookObj.length;i+=1) {
-      objVar += `<li><p><span>${bookObj[i].bookTitle}</span> by <span>${bookObj[i].arthur}</span> </p> <button id=${bookObj[i].id.toString()} onClick="removeBook(this.id)">Remove</button></li>`
-   }
-   bookList.innerHTML = objVar
-   localStorage.setItem('inputArr',JSON.stringify(bookObj))
+let filteredArr = [];
+/* eslint-disable */
+function removeBook(id) {
+  filteredArr = bookObj.filter((item) => item.id !== id);
+  bookObj = filteredArr;
+  let objVar = '';
+  for (let i = 0; i < bookObj.length; i += 1) {
+    objVar += `<li>
+    <p>
+    <span>${bookObj[i].bookTitle}</span> by <span>${bookObj[i].arthur}</span> 
+    </p> 
+    <button id=${bookObj[i].id.toString()} onClick="removeBook(${bookObj[i].id})">Remove</button>
+    </li>`;
+  }
+  bookList.innerHTML = objVar;
+  localStorage.setItem('inputArr', JSON.stringify(bookObj));
 }
 
-const outputArr = JSON.parse(localStorage.getItem('inputArr'))
- if(outputArr) {
-   bookObj = outputArr
+const outputArr = JSON.parse(localStorage.getItem('inputArr'));
+if (outputArr) {
+  bookObj = outputArr;
+} else if (filteredArr) {
+  bookObj = filteredArr;
 }
-else if (filteredArr){
-   bookObj = filteredArr
+let objVar = '';
+for (let i = 0; i < bookObj.length; i += 1) {
+  objVar += `<li><p><span>${bookObj[i].bookTitle}</span> by <span>${bookObj[i].arthur}</span> </p> <button id=${bookObj[i].id.toString()} onClick="removeBook(${bookObj[i].id})">Remove</button></li>`;
 }
-objVar = ''
-for(let i = 0;i < bookObj.length;i+=1) {
-    objVar += `<li><p><span>${bookObj[i].bookTitle}</span> by <span>${bookObj[i].arthur}</span> </p> <button id=${bookObj[i].id.toString()} onClick="removeBook(this.id)">Remove</button></li>`
- }
- bookList.innerHTML = objVar
-
+bookList.innerHTML = objVar;
